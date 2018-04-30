8e519b6f3da2287087fe261c79a23bceb5aeb413
Bryce Klinker <bklinker@pillartechnology.com>
2018-04-29 21:27:44 -0500

    Initialized repository for spring boot using gradle

---
+++ src/main/java/com/spring/springtoangular/SpringToAngularApplication.java
@@ -0,0 +1,12 @@
+package com.spring.springtoangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringToAngularApplication.class, args);
+	}
+}
